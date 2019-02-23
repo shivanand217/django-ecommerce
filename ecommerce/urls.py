a758dcb751d5684676bb75e0e7d4d5f8f9ca9764
shiv anand <shivakp2111@gmail.com>
2019-02-23 12:55:47 +0530

    home page views files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import *
+
 urlpatterns = [
+    url(r'^$', home_page),
     url(r'^admin/', admin.site.urls),
 ]
