4e7659b10d56da11336d12884ba8353c454494b0
shiv anand <shivakp2111@gmail.com>
2019-03-03 11:47:14 +0530

    added products app and their models migrations

--- products/views.py
+++ products/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from products.models import *
 
 # Create your views here.
+# Add the views created here to your urls.py
+
+# Class based views
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    # to get context in class based views
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        # print(context)        
+        return context
+
+# function based views
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
+
